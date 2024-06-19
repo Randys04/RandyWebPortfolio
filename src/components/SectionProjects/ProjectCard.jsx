d152c061c554a1d9ce9b941917af7f513899660b
Randy04 <stevenmoya02@gmail.com>
2024-06-19 11:34:21 -0600

    Logo added
    Project cards adjustments

--- src/components/SectionProjects/ProjectCard.jsx
+++ src/components/SectionProjects/ProjectCard.jsx
@@ -7,20 +7,20 @@ function ProjectCard({projectName, images, techImages, description, urlCode, url
 			<div className="div-project-name">
 				<h3>{projectName}</h3>
 			</div>
-			<div className="div-project-images">
+			<div className="div-project-images project-card-sections">
 				{images.map((image, index) => (
           <img key={index} className="project-image" src={image} alt={`Project ${index}`} />
         ))}
 			</div>
-			<div className="div-project-description">
+			<div className="div-project-description project-card-sections">
 				<p>{description}</p>
 			</div>
-			<div className="div-project-technologies">
+			<div className="div-project-technologies project-card-sections">
 				{techImages.map((image, index) => (
           <img key={index} className="tech-images" src={image} alt={`tech ${index}`} />
         ))}
 			</div>
-			<div className="div-project-links">
+			<div className="div-project-links project-card-sections">
 				<a href={urlDemo} target="_blank">
 					<button disabled={!urlDemo}>Live Demo</button>
 				</a>
